d1d15d325c2ce69bf07d762352c4fdceac481cbf
Virali <56487046+Virali@users.noreply.github.com>
2019-12-06 18:10:00 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -3,6 +3,7 @@
 const srchbtn = document.getElementById("srchbtn");
 const showmorebtn = document.getElementById("showmorebtn");
 let index = 0;
+localStorage.setItem('favoriteData', JSON.stringify([]));
 
 function displaySearch(cityname) {
     localStorage.setItem('city',JSON.stringify(cityname));
@@ -33,7 +34,7 @@ function displaySearch(cityname) {
 }
 
 async function getData(cityname, pageNumber) {
-    if(pageNumber === undefined) pageNumber = 1;
+    if(!pageNumber) pageNumber = 1;
     let url = `https://cors-anywhere.herokuapp.com/https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=rent&place_name=${cityname.toLowerCase()}&page=${pageNumber}&number_of_results=5`;
     console.log(url);
     let response = await fetch(url);
@@ -44,17 +45,15 @@ async function getData(cityname, pageNumber) {
 }
 
 function displayElem(elem) {
-    let item = document.createElement('div'),
-        content = document.createElement('div'),
-        content_title = document.createElement('div'),
-        content_keywords = document.createElement('div'),
-        image = document.createElement('img'),
-        info = document.createElement('div'),
-        wrap = document.getElementById('wrap'),
-        emptystar_image = document.getElementById('emptystar'),
-        info_buttonbox = document.createElement('button');
-
-    
+    const item = document.createElement('div');
+    const content = document.createElement('div');
+    const content_title = document.createElement('div');
+    const content_keywords = document.createElement('div');
+    const image = document.createElement('img');
+    const info = document.createElement('div');
+    const wrap = document.getElementById('wrap');
+    const emptystar_image = document.createElement('image');
+    const info_button_box = document.createElement('a');
 
 
     item.className = 'list_item_new';
@@ -73,9 +72,14 @@ function displayElem(elem) {
     content.appendChild(content_title);
     content.appendChild(content_keywords);
 
-    // info_buttonbox.textContent = "\u2606";
-    
-    // info.appendChild(info_buttonbox);
+    emptystar_image.className = 'emptystar';
+    //emptystar_image.content = url("https://www.clipartwiki.com/clipimg/detail/239-2393343_tattoo-simple-star-tattoo-design.png");
+    //emptystar_image.style.height = "44px";
+    info_button_box.appendChild(emptystar_image);
+    info_button_box.textContent = "Add To Favorites";
+    info_button_box.onclick = `addToFavorite(${elem});`;
+
+    info.appendChild(info_button_box);
     info.appendChild(emptystar_image);
 
     item.appendChild(image);
@@ -90,6 +94,14 @@ function displayRow(array) {
     }
     localStorage.setItem('requestNumber', JSON.parse(localStorage.getItem('requestNumber'))+1);
 }
+
+function addToFavorite(objElem) {
+    let arrayInStorage =JSON.parse(localStorage.getItem('favoriteData'));
+
+    arrayInStorage.push(objElem);
+    console.log('pushed');
+}
+
 
 srchbtn.addEventListener('click', () => displaySearch(document.getElementsByName('textbox')[0].value));
 showmorebtn.addEventListener('click', () => {
